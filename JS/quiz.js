4be9813afd21c6789296c467dc0c5b7656657bbe
Emily Pereira <emily.i.j.pereira@gmail.com>
2021-11-29 17:23:03 +0000

    adding content to park pages and styling inc styling for mobile

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,23 +1,8 @@
-/* 
-  1. Store correct answers
-   - When quiz begins, no answers are correct
-*/
-
 let answer = 0;
-
-// 2. Store the rank of a player
 let rank;
-
-
-
-// 3. Select the <main> HTML element
 const body = document.querySelector(".quiz-answers");
 
-/*
-  4. Ask at least 5 questions
-   - Store each answer in a variable
-   - Keep track of the number of correct answers
-*/
+
 
 const aone = prompt("Which park has a roller coaster that goes upside down?")
 if (aone.toUpperCase() === "HOLLYWOOD STUDIOS"){
@@ -40,13 +25,6 @@ if (afive.toUpperCase() === "ANIMAL KINGDOM"){
   answer += 1;
 }
 
-/*
-  5. Rank player based on number of correct answers
-   - 5 correct = Gold
-   - 3-4 correct = Silver
-   - 1-2 correct = Bronze
-   - 0 correct = No crown
-*/
 
 if (answer === 5) {
   rank = "a Gold";
@@ -56,8 +34,6 @@ if (answer === 5) {
     rank = "a Bronze";
   } else { rank = "not quite a";}
 
-
-// 6. Output results to the <main> element
 body.innerHTML = `
 <h2>You got ${answer} out of 5 questions.</h2>
 <p>You are ${rank} Disney Fan</p> 
