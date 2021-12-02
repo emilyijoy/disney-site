fbf7ab2ed5d4d5948202d2ad3c35e9aadc245670
Emily Pereira <emily.i.j.pereira@gmail.com>
2021-12-02 21:34:31 +0000

    js behind button, styling stuff

---
+++ JS/which-park.js
@@ -0,0 +1,19 @@
+let numberGen = Math.floor(Math.random() * 4) + 1;
+let park;
+const HTMLResult = document.querySelector(".result");
+
+if (numberGen === 1) {
+    park = "Magic Kingdom"
+} else if (numberGen === 2) {
+    park = "EPCOT"
+} else if (numberGen === 3) {
+    park = "Hollywood Studios"
+} else if (numberGen === 4) {
+    park = "Animal Kingdom"
+} else { park = "idk, try Universal"}
+
+HTMLResult.innerHTML = `
+<h2>You should go to ${park}!</h2>
+`;
+
+console.log(park)
