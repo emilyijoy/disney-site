7ac81400f325340e71aabc5cde600e979baa41ac
Emily Pereira <emily.i.j.pereira@gmail.com>
2021-12-05 16:32:27 +0000

    day counter made and working in log

---
+++ JS/park-counter.js
@@ -0,0 +1,23 @@
+console.log("Hello!")
+
+var titleDiv = document.getElementsByClassName("title") [0];
+var pageTitle = titleDiv.innerHTML;
+var dateOpen;
+
+
+if (pageTitle === "Magic Kingdom") {
+    dateOpen = new Date("10/1/1971");
+} else if (pageTitle === "Epcot") {
+    dateOpen = new Date("10/1/1982");
+} else if (pageTitle === "Hollywood Studios") {
+    dateOpen = new Date("5/1/1989");
+} else if (pageTitle === "Animal Kingdom") {
+    dateOpen = new Date("4/22/1998");
+} else {dateOpen = new Date("12/1/2021");}
+
+
+var dateNow = new Date();
+var dateDif = Math.abs (dateNow - dateOpen);
+var dayDif = Math.floor (dateDif / (1000 * 3600 * 24));
+
+console.log(dayDif);
