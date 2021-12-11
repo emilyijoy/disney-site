98ce108a7f2f59574132e70e5109a70f4451eb45
Emily Pereira <emily.i.j.pereira@gmail.com>
2021-12-11 19:32:58 +0000

    progress on fact finder

--- JS/facts.js
+++ JS/facts.js
@@ -1,4 +1,23 @@
 function factFinder () {
-
+    let numberGen = Math.floor(Math.random() * 7) + 1;
+    let park;
+    let fact;
+    const HTMLResult = document.querySelector(".result");
 
 }
+
+/* the facts
+mk-
+Some of the art in Cinderella's Castle was made using 14 karat gold
+While there are many American flags in Main Street USA, only the one in the central flagpole is real. The rest miss some stars/stripes so they do not have to be taken down every night as the US Flag Code advises.
+Cast members (Disney employees) get around the park via a massive system of underground tunnels with exits in each land.
+There is a special suite inside Cinderella's Castle that you can only stay in via a competition held by Disney.
+There are bins every 30 steps in the park as Walt himself found that guests were willing to walk 30 steps before considering littering
+While Magic Kingdom is massive (at 107 acres) it's smaller than the Animal Kingdom parking lot!
+Forced perspective is used a lot in the parks to make things appear larger than they are, one use of this is Beast's castle in Fantasyland.
+ep-
+hs-
+This is the only park in WDW to have an inversion on a coaster (on the Rock 'n' Roller Coaster Starring Aerosmith).
+
+ak-
+*/
