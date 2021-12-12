e932911df9fb28eff2151c02262c6dd373b6cde7
Emily Pereira <emily.i.j.pereira@gmail.com>
2021-12-12 20:32:24 +0000

    progress on facts

--- JS/facts.js
+++ JS/facts.js
@@ -1,6 +1,8 @@
+var titleDiv = document.getElementsByClassName("title") [0];
+var pageTitle = titleDiv.innerHTML;
+
 function factFinder () {
     let numberGen = Math.floor(Math.random() * 7) + 1;
-    let park;
     let fact;
     const HTMLResult = document.querySelector(".result");
 
@@ -16,8 +18,25 @@ There are bins every 30 steps in the park as Walt himself found that guests were
 While Magic Kingdom is massive (at 107 acres) it's smaller than the Animal Kingdom parking lot!
 Forced perspective is used a lot in the parks to make things appear larger than they are, one use of this is Beast's castle in Fantasyland.
 ep-
+Though a new country has not been added to the world showcase since 1988, there have been many countries for which a land was planned and promotional material was made, including Spain and Israel.
+The countries currently represented in the world showcase are Mexico, Norway, China, Germany, America, Italy, Morocco, Japan, France, the UK and Canada.
+Test Track is the fastest ride in the park, reaching speeds of 65mph.
+Epcot hosts both the the Food and Wine festival and the Flower and Garden Festival.
+The crops grown in the greenhouse area of The Land pavillion are used to feed guests across the resort.
+Epcot is twice the size of the Magic Kingdom.
+Spaceship Earth doubles as both one of the opening day attractions at the park and the park icon (though many refer to it as the 'Epcot ball').
 hs-
 This is the only park in WDW to have an inversion on a coaster (on the Rock 'n' Roller Coaster Starring Aerosmith).
-
+Though it is no longer there, the mascot of the park used to be a giant sorcer Mickey hat.
+Hollywood Studios was almost merely a pavillion at Epcot but then CEO Michael Eisner decided that there should be an entire park dedicated to the theme.
+The park was originally named Disney's MGM Studios and had a working animation studio that made and produced several classic Disney films, incluiding Lilo & Stitch.
+While hidden Mickeys are common across all Disney parks, Hollywood Studios is home to one of the largest that can only be seen from the sky.
+Hollywood Studios is the only place you can find Audrey Hepburn's handprints! Grauman's Chinese Theater had never asked her for them and when they realised this oversight and reached out to her to rectify it she declined.
+The All-New Mickey Mouse Club was filmed in the park featuring stars such as Justin Timberlake, Ryan Gosling and Britney Spears.
 ak-
+Animal Kingdom is the newest of the Disney World parks.
+There was originally supposed to be a ride about unicorns and another about dragons in the parks, hence why you can see unicorns and dragons on some sinage around the park.
+
+
+
 */
